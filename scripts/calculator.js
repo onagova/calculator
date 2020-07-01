a843e5ba9b82eace7abc1df0687abef856dee775
Tanat Cheurthai <naepanda@hotmail.com>
2020-07-01 14:50:17 +0700

    Add operate function

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,3 +1,27 @@
+const OPERATOR_ADD = '&#43;';
+const OPERATOR_SUBTRACT = '&#8722;';
+const OPERATOR_MULTIPLY = '&#215;';
+const OPERATOR_DIVIDE = '&#247;';
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case OPERATOR_ADD:
+            return add(a, b);
+        
+        case OPERATOR_SUBTRACT:
+            return subtract(a, b);
+
+        case OPERATOR_MULTIPLY:
+            return multiply(a, b);
+
+        case OPERATOR_DIVIDE:
+            return divide(a, b);
+
+        default:
+            return;
+    }
+}
+
 function add(a, b) {
     return a + b;
 }
