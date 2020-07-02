b59ea3d1cb7340cc9ae3d9405ebec00cdb827e6c
Tanat Cheurthai <naepanda@hotmail.com>
2020-07-02 14:34:46 +0700

    Add clear button, Improve input validation

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -2,6 +2,7 @@ const OPERATOR_ADD = '0x002B';
 const OPERATOR_SUBTRACT = '0x2212';
 const OPERATOR_MULTIPLY = '0x00D7';
 const OPERATOR_DIVIDE = '0x00F7';
+const ERROR_DIVIDED_BY_ZERO = 0;
 
 startCalculator();
 
@@ -17,7 +18,8 @@ function setupButtons(expression) {
         button.addEventListener('click', e => {
             const char = e.target.getAttribute('data-char');
             appendChar(expression, char);
-            updateDisplay(expression);
+            updateInputDisplay(expression);
+            displayForwardEvaluation(expression);
         });
     });
 
@@ -26,20 +28,35 @@ function setupButtons(expression) {
         button.addEventListener('click', e => {
             const operator = e.target.getAttribute('data-charcode');
             appendOperator(expression, operator);
-            updateDisplay(expression);
+            updateInputDisplay(expression);
         })
     });
 
     const equalsButton = document.querySelector('.equals-button');
     equalsButton.addEventListener('click', () => {
-        const trimmed = getTrimmed(expression);
-        const evaluated = getEvaluated(trimmed);
+        const evaluated = getEvaluated(expression);
 
-        if (trimmed != evaluated) {
-            expression = getUntrimmed(evaluated);
+        switch (evaluated) {
+            case null:
+                return;
+
+            case ERROR_DIVIDED_BY_ZERO:
+                updateEvaluatedDisplay('Divide by zero is against the rule and you know it.');
+                return;
+
+            default:
+                break;
         }
+        
+        expression = getUntrimmed(evaluated);
+        updateInputDisplay(expression);
+        updateEvaluatedDisplay(null);
+    });
 
-        updateDisplay(expression);
+    const clearButton = document.querySelector('.clear-button');
+    clearButton.addEventListener('click', () => {
+        expression = [''];
+        updateInputDisplay(expression);
     });
 }
 
@@ -48,36 +65,80 @@ function appendChar(expression, char) {
 }
 
 function appendOperator(expression, operator) {
+    if (expression[expression.length - 1] == String.fromCharCode(OPERATOR_SUBTRACT)) return;
+
     if (expression[expression.length - 1] == '') {
-        if (expression.length == 1 && operator == OPERATOR_SUBTRACT) {
-            expression.shift();
-        } else {
-            return;
+        if (expression.length > 1) {
+            if (operator == OPERATOR_SUBTRACT) {
+                switch (expression[expression.length - 2]) {
+                    case String.fromCharCode(OPERATOR_ADD):
+                        expression[expression.length - 2] = String.fromCharCode(PERATOR_SUBTRACT);
+                        break;
+
+                    case String.fromCharCode(OPERATOR_SUBTRACT):
+                        break;
+
+                    default:
+                        appendChar(expression, String.fromCharCode(OPERATOR_SUBTRACT));
+                        break;
+                }
+            } else {
+                expression[expression.length - 2] = String.fromCharCode(operator);
+            }
+        } else if (operator == OPERATOR_SUBTRACT) {
+            appendChar(expression, String.fromCharCode(OPERATOR_SUBTRACT));
         }
+
+        return;
     }
 
     expression.push(String.fromCharCode(operator));
     expression.push('');
 }
 
-function updateDisplay(expression) {
-    const display = document.querySelector('.calculator-display');
+function updateInputDisplay(expression) {
+    const display = document.querySelector('#calculator-input-display');
     display.textContent = expression.join('');
 }
 
+function updateEvaluatedDisplay(str) {
+    const display = document.querySelector('#calculator-evaluated-display');
+    display.textContent = str;
+}
+
+function displayForwardEvaluation(expression) {
+    let evaluated = getEvaluated(expression);
+
+    switch (evaluated) {
+        case null:
+        case ERROR_DIVIDED_BY_ZERO:
+            updateEvaluatedDisplay(null);
+            return;
+
+        default:
+            break;
+    }
+    
+    evaluated = getUntrimmed(evaluated);
+    updateEvaluatedDisplay(evaluated[0]);
+}
+
 function getTrimmed(expression) {
-    const trimmed = Array.from(expression);
+    let trimmed = Array.from(expression);
 
-    if (trimmed.length <= 1) return trimmed;
-
-    if (trimmed[0] == String.fromCharCode(OPERATOR_SUBTRACT)) {
-        trimmed[1] = `-${trimmed[1]}`;
-        trimmed.shift();
+    if (trimmed[trimmed.length - 1] == String.fromCharCode(OPERATOR_SUBTRACT)) {
+        trimmed[trimmed.length - 1] = '';
     }
 
-    if (trimmed[trimmed.length - 1] == '') {
-        trimmed.splice(-2, 2);
+    if (trimmed.length > 2 && trimmed[trimmed.length - 1] == '') {
+        trimmed = trimmed.slice(0, -2);
     }
+
+    trimmed = trimmed.map(str => {
+        return str[0] == String.fromCharCode(OPERATOR_SUBTRACT) && str.length > 1 ?
+                `-${str.slice(1)}` :
+                str;
+    });
 
     return trimmed;
 }
@@ -87,19 +148,27 @@ function getUntrimmed(expression) {
 
     if (untrimmed[0] >= 0) return untrimmed;
 
-    untrimmed[0] = Math.abs(expression[0]);
-    untrimmed.unshift(String.fromCharCode(OPERATOR_SUBTRACT));
+    untrimmed[0] = `${String.fromCharCode(OPERATOR_SUBTRACT)}${Math.abs(untrimmed)}`;
 
     return untrimmed;
 }
 
 function getEvaluated(expression) {
-    const evaluating = Array.from(expression);
+    const trimmed = getTrimmed(expression);
 
-    const addIndex = evaluating.indexOf(String.fromCharCode(OPERATOR_ADD));
-    const subIndex = evaluating.indexOf(String.fromCharCode(OPERATOR_SUBTRACT));
-    const mulIndex = evaluating.indexOf(String.fromCharCode(OPERATOR_MULTIPLY));
-    const divIndex = evaluating.indexOf(String.fromCharCode(OPERATOR_DIVIDE));
+    if (trimmed.length <= 1) return null;
+
+    return evaluate(trimmed);
+}
+
+function evaluate(expression) {
+    const divIndex = expression.indexOf(String.fromCharCode(OPERATOR_DIVIDE));
+
+    if (divIndex >= 0 && expression[divIndex + 1] == '0') return ERROR_DIVIDED_BY_ZERO;
+
+    const addIndex = expression.indexOf(String.fromCharCode(OPERATOR_ADD));
+    const subIndex = expression.indexOf(String.fromCharCode(OPERATOR_SUBTRACT));
+    const mulIndex = expression.indexOf(String.fromCharCode(OPERATOR_MULTIPLY));
 
     let index, operator;
 
@@ -132,13 +201,13 @@ function getEvaluated(expression) {
         index = subIndex;
         operator = OPERATOR_SUBTRACT;
     } else {
-        return evaluating;
+        return expression;
     }
 
-    const operated = operate(operator, +evaluating[index - 1], +evaluating[index + 1]);
-    evaluating.splice(index - 1, 3, operated.toString());
+    const operated = operate(operator, +expression[index - 1], +expression[index + 1]);
+    expression.splice(index - 1, 3, operated.toString());
 
-    return getEvaluated(evaluating);
+    return evaluate(expression);
 }
 
 function operate(operator, a, b) {
